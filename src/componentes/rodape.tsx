87a854605779f2626cd01b1b305b14a504310865
Lucas Mateus Soares de Lima <lucasmslima1@gmail.com>
2024-08-16 10:22:23 -0300

    Atualização
    Todos os componentes reordenados

---
+++ src/componentes/rodape.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "../estilos/rodape.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+function Rodape() {
+  return (
+    <div className='rodape'>
+      <div className='rodape-logo'>
+        {/* Adicione aqui o componente ou imagem da logo */}
+      </div>
+      <div className='rodape-conteudo'>
+        <div className='rodape-info'>
+          <h2 className='rodape-titulo'>Sobre Mim</h2>
+          <p className='rodape-texto'>
+            Desenvolvedor Full Stack e Designer com paixão por criar soluções digitais
+            inovadoras. Entre em contato para colaborações e projetos.
+          </p>
+        </div>
+        <div className='rodape-contato'>
+          <p className='rodape-texto'>
+            <a
+              href='tel:+5514988025296'
+              className='rodape-link'
+            >
+              <FontAwesomeIcon
+                icon={faWhatsapp}
+                className='rodape-icon-whats'
+              />
+              &nbsp; Telefone: (14) 98802-5296
+            </a>
+          </p>
+          <p className='rodape-texto'>
+            <a
+              href='mailto:lucasmslima1@gmail.com'
+              className='rodape-link'
+            >
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className='rodape-icon-email'
+              />
+              &nbsp; Email: lucasmslima1@gmail.com
+            </a>
+          </p>
+        </div>
+        <div className='rodape-redes-sociais'>
+          <a
+            href='https://www.linkedin.com/in/lucaslimna'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='rodape-link'
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className='rodape-icon'
+            />
+          </a>
+          <a
+            href='https://github.com/lukaslimna1'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='rodape-link'
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className='rodape-icon'
+            />
+          </a>
+          <a
+            href='https://www.instagram.com/lucaslimna'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='rodape-link'
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className='rodape-icon'
+            />
+          </a>
+        </div>
+      </div>
+      <div className='rodape-rodape'>
+        <p className='rodape-copyright'>
+          © 2024 Lucas Lima. Todos os direitos reservados.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Rodape;
--- src/paginas/home.tsx
+++ src/paginas/home.tsx
@@ -1,116 +1,162 @@
 import React from "react";
 import "../estilos/home.css";
 import Menu from "../componentes/menu";
+import Rodape from "../componentes/rodape";
 
 function Home() {
   return (
     <div className='home-background'>
       <Menu />
+      {/*Primera Parte */}
       <div className='home-cima'>
+        <div className='home-titulos'>
+          <div className='home-titulos-principal'>
+            <h1 className='home-titulos-principalh1'>
+              <span className='principal'>D</span>esenvolvedor <br /> FullStack | Design
+            </h1>
+            <p className='home-titulos-principalp'>
+              Desenvolvendo com paixão, projetando com precisão, seu próximo projeto
+              começa aqui.
+            </p>
+          </div>
+          <div className='home-titulos-secundarios'>
+            <div className='home-titulos-FD'>
+              <h3 className='home-titulo-auxh3'> Desenvolvedor Full Stack & Designer</h3>
+              <p className='home-titulo-auxp'>
+                "Formado em Análise de Sistemas e Design Gráfico, ofereço soluções
+                digitais integradas que combinam funcionalidade e estética inovadora."
+              </p>
+            </div>
+            <div className='home-titulos-HE'>
+              <h3 className='home-titulo-auxh3'>Habilidades e Experiência</h3>
+              <p className='home-titulo-auxp'>
+                "Experiência em desenvolvimento Full Stack e design de interfaces, com um
+                histórico de criação de produtos digitais eficazes e visualmente
+                atraentes."
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='home-foto'>
+          <div className='home-perfil'></div>
+        </div>
+      </div>
+
+      {/*Segunda Parte */}
+
+      <div className='home-info-fundo'>
         <div>
-          <h1 className='home-titulo'>
-            Desenvolvedor <br /> Full Stack | Design
-          </h1>
-          <div>
-            <p className='home-subtitulo'>
-              "Desenvolvendo com paixão, projetando com precisão,
-              <br /> seu próximo projeto começa aqui."
-            </p>
+          <div className='home-info'>
+            <div className='home-info1'>
+              <h2 className='home-infoh2'>
+                <span className='principal'>D</span>esign
+              </h2>
+              <p className='home-infop'>
+                "Embora não passe horas ajustando pixels em um artboard, estou
+                profundamente envolvido na criação de experiências digitais impactantes.
+                Com formação técnica em Assistência de Produção Gráfica e Web Design,
+                minha paixão está em otimizar estilos e layouts para proporcionar uma
+                experiência de usuário fluida e atraente."
+              </p>
+            </div>
+            <div className='home-info2'></div>
           </div>
-        </div>
-        <div className='home-perfil'></div>
-        <div className='home-chamada-fundo'>
-          <div className='home-chamada1'>
-            <h3 className='chamada-h3'>Full Stack e Designer</h3>
-            <p className='chamada-p'>
-              "Formado em Análise de Sistemas e Design Gráfico, criando soluções digitais
-              completas e impactantes."
-            </p>
-          </div>
-          <div className='home-chamada2'>
-            <h3 className='chamada-h3'>Habilidades e Experiência</h3>
-            <p className='chamada-p'>
-              "Especialista em desenvolvimento Full Stack e design de interfaces.
-              Experiência em entregar produtos digitais inovadores e funcionais."
-            </p>
+          <div className='home-info'>
+            <div className='home-info1'></div>
+            <div className='home-info2'>
+              <h2 className='home-infoh2'>
+                {" "}
+                <span className='principal'>D</span>esenvolvimento
+              </h2>
+              <p className='home-infop'>
+                "Como Tecnólogo em Análise e Desenvolvimento de Sistemas e Técnico em
+                Desenvolvimento Full Stack, uso ferramentas e tecnologias como React.Js,
+                Node.Js e MySQL para criar soluções ágeis e escaláveis. Meu objetivo é
+                entregar aplicações eficientes, priorizando desempenho e escalabilidade."
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className='home-design-dev-fundo'>
-        <div className='home-design'>
-          <h2 className='home-dd-titulo'>Design</h2>
-          <p className='home-dd-texto'>
-            "Embora não passe horas ajustando pixels em um artboard, estou profundamente
-            envolvido na criação de experiências digitais impactantes. Com formação
-            técnica em Assistência de Produção Gráfica e Web Design, minha paixão está em
-            otimizar estilos e layouts para proporcionar uma experiência de usuário fluida
-            e atraente."
-          </p>
-        </div>
-        <div className='home-dev'>
-          <h2 className='home-dd-titulo'>Desenvolvimento</h2>
-          <p className='home-dd-texto'>
-            "Como Tecnólogo em Análise e Desenvolvimento de Sistemas e Técnico em
-            Desenvolvimento Full Stack, uso ferramentas e tecnologias como React.Js,
-            Node.Js e MySQL para criar soluções ágeis e escaláveis. Meu objetivo é
-            entregar aplicações eficientes, priorizando desempenho e escalabilidade."
-          </p>
-        </div>
-      </div>
+
+      {/*Terceira Parte */}
+
       <div className='home-formacao'>
-        <div className='home-container-formacao'>
-          <h1 className='home-formacao-titulo'>Ao longo da minha formação !</h1>
-          <p className='home-formacao-texto'>
+        <div className='home-formacao-texto'>
+          <h1 className='home-formacaoh1'>Ao longo da minha formação !</h1>
+          <p className='home-formacaop'>
             Desenvolvi habilidades em design e desenvolvimento de sistemas, abrangendo
             desde a criação de interfaces gráficas até o desenvolvimento de aplicações web
             completas. Com cursos em Desenvolvimento Full Stack, Web Design e Assistência
             de Produção Gráfica, estou preparado para construir soluções digitais eficazes
             e impactantes.
           </p>
-          <p className='home-formacao-texto'>
+          <p className='home-formacaop'>
             Atualmente, estou focado em aprimorar minhas habilidades práticas em
             tecnologias como React.Js, Node.Js e MySQL, aplicando o conhecimento adquirido
             em projetos acadêmicos e cursos técnicos.
           </p>
-          <p className='home-formacao-texto'>
+          <p className='home-formacaop'>
             Busco oportunidades para aplicar minha formação em projetos reais, com o
             objetivo de criar experiências digitais que sejam tanto funcionais quanto
             esteticamente agradáveis.
           </p>
-          <p className='home-formacao-texto'>
+          <p className='home-formacaop'>
             Em minha jornada, tenho me dedicado a aprender e aplicar conceitos de UX/UI
             para desenvolver interfaces intuitivas e de alta qualidade, sempre com o
             compromisso de entregar soluções que atendam às necessidades e expectativas
             dos usuários.
           </p>
         </div>
-        <div className='home-img-formacao'></div>
+        <div className='home-formacao-imagem'></div>
       </div>
-      <div className='home-links-container'>
-        <div className='home-links-esquerdo'>
-          <h1 className='home-links-titulo'>Veja o código em Ação !</h1>
-          <p className='home-links-paragrafo'>
-            "Desenvolvo o código que dá vida às minhas interfaces, garantindo uma
-            experiência de usuário suave e eficiente."
-          </p>
-          <button className='home-link-btn'>
-            {" "}
-            <span>Veja Mais</span>{" "}
-          </button>
-        </div>
-        <div className='home-links-wave'></div>
-        <div className='home-links-direito'>
-          <h1 className='home-links-titulo'>Dê uma olhada nos meus Designs</h1>
-          <p className='home-links-paragrafo'>
-            "Crio interfaces e telas para aplicativos e soluções digitais, tornando cada
-            projeto funcional e visualmente marcante."
-          </p>
-          <button className='home-link-btn'>
-            {" "}
-            <span> Veja Mais</span>{" "}
-          </button>
+
+      {/*Quarta Parte */}
+      <div>
+        <div className='home-link'>
+          <div className='home-link-esquerdo'>
+            <div className='home-link-textos'>
+              <h1 className='home-linkh1'>
+                Veja o código <br />
+                em Ação !
+              </h1>
+              <p className='home-linkp'>
+                "Desenvolvo o código que dá vida às minhas interfaces, garantindo uma
+                experiência de usuário suave e eficiente."
+              </p>
+            </div>
+            <div className='home-link-btn'>
+              <button className='home-linkbtn'>
+                {" "}
+                <span>Veja Mais</span>{" "}
+              </button>
+            </div>
+          </div>
+          {/* INICIO - Barra de separação */}
+          <div className='home-link-meio'></div>
+          {/* FINAL - Barra de separação */}
+          <div className='home-link-direito'>
+            <div className='home-link-textos'>
+              <h1 className='home-linkh1'>
+                Dê uma olhada
+                <br />
+                nos meus Designs
+              </h1>
+              <p className='home-linkp'>
+                "Crio interfaces e telas para aplicativos e soluções digitais, tornando
+                cada projeto funcional e visualmente marcante."
+              </p>
+            </div>
+            <div className='home-link-btn'>
+              <button className='home-linkbtn'>
+                {" "}
+                <span>Veja Mais</span>{" "}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
+      <Rodape />
     </div>
   );
 }
